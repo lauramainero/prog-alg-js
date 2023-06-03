5afeca2ccbbbf8a32bf5122ae45300765b49fbe5
lauramainero <mainerolaura@gmail.com>
2023-06-03 16:30:44 -0300

    fix: button

--- ch_4/ex4_1/ex4_1.js
+++ ch_4/ex4_1/ex4_1.js
@@ -3,18 +3,21 @@ function mostrarTabuada() {
     var outTabuada = document.getElementById("outTabuada");
 
     var numero = Number(inNumero.value);
-    var resposta = "";
-
-    if (numero == 0 || inNumero.value == "" || isNaN(inNumero)) {
-        alert("Informe um número válido.")
+    
+    if (numero == 0 || inNumero.value == "" || isNaN(numero)) {
+        alert("Informe um número válido.");
         inNumero.value = "";
         inNumero.focus();
         return;
     }
     
+    var resposta = "";
+
     for (var i = 1; i <= 10; i++) {
-        resposta = resposta + numero + "x" + i + " = " + numero * i + "\n"; 
+        resposta = resposta + numero + " x " + i + " = " + numero * i + "\n";
     }
+
+    outTabuada.textContent = resposta;
 }
-var btMostrar = document.getElementById(btMostrar);
+var btMostrar = document.getElementById("btMostrar");
 btMostrar.addEventListener("click", mostrarTabuada);
